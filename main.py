e2e163d2c3ef500a0a500ff3a3a98b0199776795
John Doe <johndoe@example.com>
2024-03-17 12:26:59 +0100

    'OK'

---
+++ main.py
@@ -0,0 +1,16 @@
+import time
+import adafruit_dht
+import board
+
+dht_device = adafruit_dht.DHT11(board.D4)
+temperature_c = dht_device.temperature
+    
+if temperature_c >25 :
+    lcd.putstr('Prenez un verre d eau')
+
+    if temperature_c <25 :
+    lcd.putstr('Couvrez-vous')
+
+
+
+       
